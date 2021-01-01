9141e710023390cf6e8ff9092a95d2cade0eb768
Felipe <jsolerrosas@gmail.com>
2021-01-01 18:02:30 -0500

    Inicio de proyecto

---
+++ src/js/componentes.js
@@ -0,0 +1,9 @@
+import '../CSS/componentes.css';
+export const saludar = ( nombre ) => {
+    console.log( 'Creando etiqueta h1' );
+
+    const h1 = document.createElement( 'h1' );
+    h1.innerText = `Hola, ${ nombre } ¿Cómo estás?`;
+
+    document.body.append(h1);
+};
